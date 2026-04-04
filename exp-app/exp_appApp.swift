fe6824dc009fc010eb273576065225ae2b320527
Kasun <kasunvimu.dev@gmail.com>
2026-04-04 23:28:22 +0200

    Initial Commit

---
+++ exp-app/exp_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  exp_appApp.swift
+//  exp-app
+//
+//  Created by Kasun Vimukthi on 04.04.26.
+//
+
+import SwiftUI
+
+@main
+struct exp_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
